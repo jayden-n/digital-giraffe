eb066e1ec0a95af7a764acedd9e860568e3f01f6
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-04-22 14:22:22 -0400

    refactor: apply responsive app layout w/ shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 export default function Home() {
-	return <div>hello</div>;
+	return (
+		<MaxWidthWrapper>
+			<div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+				hellooo
+			</div>
+		</MaxWidthWrapper>
+	);
 }
