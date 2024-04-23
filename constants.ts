5e22c6e3e3d3614eaed2b5f07f8e709979c8bad6
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-04-23 12:41:24 -0400

    feat: create hero section

---
+++ constants.ts
@@ -0,0 +1,25 @@
+import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
+
+export const perks = [
+	{
+		id: 1,
+		name: "Instant Delivery",
+		Icon: ArrowDownToLine,
+		description:
+			"Get your assets delivered to your email in seconds and download them right away.",
+	},
+	{
+		id: 2,
+		name: "Guaranteed Quality",
+		Icon: CheckCircle,
+		description:
+			"Every asset on our platform is verified by our team to ensure our highest quality standards. Not happy? We offer a 30-day refund guarantee.",
+	},
+	{
+		id: 3,
+		name: "For the Planet",
+		Icon: Leaf,
+		description:
+			"We've pledged 1% of sales to the preservation and restoration of the natural environment.",
+	},
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,59 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+import { perks } from "../../constants";
 
 export default function Home() {
 	return (
-		<MaxWidthWrapper>
-			<div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-				hellooo
-			</div>
-		</MaxWidthWrapper>
+		<>
+			<MaxWidthWrapper>
+				<div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+					<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+						Your marketplace for high-quality{" "}
+						<span className="text-violet-600">digital assets</span>.
+					</h1>
+					<p className="mt-6 text-lg max-w-prose text-muted-foreground">
+						Welcome to PixelMarket. Every asset on our platform is verified by
+						our team to ensure our highest quality standards.
+					</p>
+					<div className="flex sm:flex-row flex-col gap-4 mt-6">
+						<Link href="/products" className={buttonVariants()}>
+							Browse Trending
+						</Link>
+						<Button variant="ghost">Our quality promise &rarr;</Button>
+					</div>
+				</div>
+
+				{/* TODO: List products */}
+			</MaxWidthWrapper>
+
+			<section className="border-t border-gray-200 bg-gray-50">
+				<MaxWidthWrapper className="py-20">
+					<div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-y-0">
+						{perks.map((perk) => (
+							<div
+								key={perk.id}
+								className="text-center md:flex md:items-start md:text-left lg:block lg:text-center"
+							>
+								<div className="md:flex-shrink-0 flex justify-center">
+									<div className="h-16 w-16 flex items-center justify-center rounded-full bg-violet-100 text-violet-900">
+										{<perk.Icon className="w-1.5/3 h-1.5/3" />}
+									</div>
+								</div>
+
+								<div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
+									<h3 className="text-base font-medium text-gray-900">
+										{perk.name}
+									</h3>
+									<p className="mt-3 text-sm text-muted-foreground">
+										{perk.description}
+									</p>
+								</div>
+							</div>
+						))}
+					</div>
+				</MaxWidthWrapper>
+			</section>
+		</>
 	);
 }
