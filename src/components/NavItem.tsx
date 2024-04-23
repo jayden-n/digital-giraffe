a9d91084d122a48498843c2d899bf987ba54dfb1
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-04-23 15:15:06 -0400

    feat: implement click-outside function to close the navbar for UX

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -2,6 +2,8 @@ import { PRODUCT_CATEGORIES } from "@/config";
 import { Button } from "./ui/button";
 import { ChevronDown } from "lucide-react";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 
 type Category = (typeof PRODUCT_CATEGORIES)[number];
 
@@ -29,6 +31,57 @@ const NavItem = ({ isAnyOpen, category, isOpen, handleOpen }: NavItemProps) => {
 					/>
 				</Button>
 			</div>
+
+			{/* conditional nav rendering */}
+			{isOpen ? (
+				<div
+					className={cn(
+						"absolute inset-x-0 top-full text-sm text-muted-foreground",
+						{
+							"animate-in fade-in-10 slide-in-from-left-5": !isAnyOpen,
+						},
+					)}
+				>
+					<div
+						className="absolute inset-0 top-1/2 bg-white shadow"
+						aria-hidden="true"
+					/>
+
+					<div className="relative bg-white">
+						<div className="mx-auto max-w-7xl px-8">
+							<div className="grid grid-cols-4 gap-x-8 gap-y-10 py-16">
+								<div className="col-span-4 col-start-1 grid grid-cols-3 gap-x-8">
+									{category.featured.map((item) => (
+										<div
+											key={item.name}
+											className="group relative text-base sm:text-sm"
+										>
+											<div className="relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75 group-hover:cursor-pointer">
+												<Image
+													src={item.imageSrc}
+													alt="product category image"
+													fill
+													className="object-cover object-center"
+												/>
+											</div>
+
+											<Link
+												href={item.href}
+												className="mt-6 block font-medium text-gray-900"
+											>
+												{item.name}
+											</Link>
+											<p className="mt-1" aria-hidden="true">
+												Shop now!
+											</p>
+										</div>
+									))}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			) : null}
 		</div>
 	);
 };
--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,14 +1,21 @@
 "use client";
 
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
 	const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
+	const isAnyOpen = activeIndex !== null;
+
+	const navRef = useRef<HTMLDivElement | null>(null);
+
+	useOnClickOutside(navRef, () => setActiveIndex(null)); // close navbar when click outside
+
 	return (
-		<div className="flex gap-4 h-full">
+		<div className="flex gap-4 h-full" ref={navRef}>
 			{PRODUCT_CATEGORIES.map((category, i) => {
 				const handleOpen = () => {
 					if (activeIndex === i) {
@@ -22,10 +29,11 @@ const NavItems = () => {
 
 				return (
 					<NavItem
-						key={i}
+						key={category.value}
 						category={category}
 						isOpen={isOpen}
 						handleOpen={handleOpen}
+						isAnyOpen={isAnyOpen}
 					/>
 				);
 			})}
