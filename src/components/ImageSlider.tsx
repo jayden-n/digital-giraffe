e7ee15f7920f3462dd90297cca6629feb4cc2a20
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-05-03 16:53:37 -0400

    refactor: styling bullet indicators when swiping product images

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -31,7 +31,7 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
 	}, [swiper, urls]);
 
 	const activeStyles =
-		"active:scale-[0.97] grid opacity-100 hover:scale-125 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
+		"active:scale-[0.97] grid opacity-100 hover:scale-150 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
 	const inactiveStyles = "hidden text-gray-400";
 
 	return (
