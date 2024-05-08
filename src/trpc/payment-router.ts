ca78d35a0de0b6bcd79a35ed71de095ef0bd5e01
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-05-08 13:06:33 -0400

    feat: author Stripe API integration in server

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 import { privateProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { getPayloadClient } from "../get-payload";
+import { stripe } from "../lib/stripe";
+import type Stripe from "stripe";
 
 export const paymentRouter = router({
 	createSession: privateProcedure
@@ -25,6 +27,56 @@ export const paymentRouter = router({
 				},
 			});
 
-			// TODO: create checkout session
+			// filter and only get the products with id
+			const filteredProducts = products.filter((prod) => Boolean(prod.priceId));
+
+			// creating order in database
+			const order = await payload.create({
+				collection: "orders",
+				data: {
+					_isPaid: false,
+					products: filteredProducts,
+					user: user.id,
+				},
+			});
+
+			const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
+			filteredProducts.forEach((product) => {
+				line_items.push({
+					price: product.priceId!,
+					quantity: 1, // just need 1-time transaction for digital product
+				});
+			});
+
+			line_items.push({
+				// get from "create product" from Stripe
+				price: "price_1PDsz9EYrxeyUEBNcy2YZdqR",
+				quantity: 1,
+				adjustable_quantity: {
+					enabled: false,
+				},
+			});
+
+			// ===================== STRIPE API INTEGRATION =====================
+			try {
+				const stripeSession = await stripe.checkout.sessions.create({
+					success_url: `${process.env.NEXT_SERVER_SERVER_URL}/thank-you?orderId=${order.id}`,
+					cancel_url: `${process.env.NEXT_SERVER_SERVER_URL}/cart`,
+					payment_method_types: ["card", "paypal"],
+					mode: "payment",
+					metadata: {
+						userId: user.id,
+						orderId: order.id,
+					},
+					line_items,
+				});
+
+				return { url: stripeSession.url };
+			} catch (error) {
+				console.log(error);
+
+				return { url: null };
+			}
 		}),
 });
