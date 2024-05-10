ee01ff1d2d4314cb8badb513fcee47530b929e96
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-05-10 10:47:29 -0400

    refactor(security): synchronize the user's list of products after a change has been made

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,3 +1,4 @@
+import { PrimaryActionEmailHtml } from "@/components/emails/PrimaryActionEmail";
 import { Access, CollectionConfig } from "payload/types";
 
 const adminsAndUser: Access = ({ req: { user } }) => {
@@ -15,7 +16,11 @@ export const Users: CollectionConfig = {
 	auth: {
 		verify: {
 			generateEmailHTML: ({ token }) => {
-				return `<a href='${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}'>Verify account</a>`;
+				return PrimaryActionEmailHtml({
+					actionLabel: "Verify your account",
+					buttonText: "Verify Account",
+					href: `${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}`,
+				});
 			},
 		},
 	},
